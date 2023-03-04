ccc1f62943c44852ea75046238cd8d40e706cf95
Evankhell_global <maxfedorenko2004@gmail.com>
2023-03-05 02:12:02 +0500

    first real commit and starting work with main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}//первая функция (рандомное число в диапазоне)
+function truncate(str, maxlength) {
+    if(str.length <= maxlength){return (true)}else{return (false)}
+   }//вторая функция (на длину строки)
