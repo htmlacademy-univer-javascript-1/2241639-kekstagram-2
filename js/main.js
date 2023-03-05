d460d38343045029afe35fec1c529bd6605ff77b
Evankhell_global <maxfedorenko2004@gmail.com>
2023-03-05 19:39:25 +0500

    little fix

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ function getRandomInt(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }//первая функция (рандомное число в диапазоне)
+
 function truncate(str, maxlength) {
-    if(str.length <= maxlength){return (true)}else{return (false)}
-   }//вторая функция (на длину строки)
+  if(str.length <= maxlength){return (true);}else{return (false);}
+} //вторая функция (на длину строки)
