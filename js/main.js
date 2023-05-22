815d8531818c8f9c88b293e07b018aa7a7fbe5c3
Evankhell_global <maxfedorenko2004@gmail.com>
2023-05-23 00:36:10 +0500

    12.11

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 import {indicateError} from './errors.js';
-import {showPhoto} from './miniatures.js';
+//import {showPhoto} from './miniatures.js';
 import {renderUploadForm} from './form-render.js';
 import {getData} from './api.js';
+import {showFilters} from './img-filters.js';
 
-getData((photos) => showPhoto(photos),
+getData((photos) => showFilters(photos),
   () => indicateError);
 renderUploadForm();
